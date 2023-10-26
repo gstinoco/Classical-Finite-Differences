399b4f1f890f57997604f6157817c896e64e2582
gstinoco <gstinoco@gmail.com>
2023-10-25 23:20:47 -0600

    Advection Equation.

---
+++ Advection_Equation.py
@@ -0,0 +1,39 @@
+"""
+Classical Finite Difference Schemes to solve Advection Equation.
+
+The problem to solve is:
+    u(x,y,t)_{t} + au(x,y,t)_{x} + bu(x,y,t)_{y} = 0
+
+Subject to conditions:
+    u(x,y,0) = g(x,y)
+    u(x,y,t)_{Boundary} = h(t)
+    
+All the codes were developed by:
+    Dr. Gerardo Tinoco Guerrero
+    Universidad Michoacana de San Nicolás de Hidalgo
+    gerardo.tinoco@umich.mx
+ 
+With the funding of:
+    National Council of Humanities, Sciences and Technologies, CONAHCyT (Consejo Nacional de Humanidades, Ciencias y Tecnologías, CONAHCyT). México.
+    Coordination of Scientific Research, CIC-UMSNH (Coordinación de la Investigación Científica de la Universidad Michoacana de San Nicolás de Hidalgo, CIC-UMSNH). México
+    Aula CIMNE-Morelia. México
+ 
+Date:
+    October, 2022.
+
+Last Modification:
+    September, 2023.
+"""
+
+# Library Importation
+import numpy as np
+
+def Advection_1D_FTCS(m, t, u, a):
+    x    = np.linspace(0,1,m)                                               # Mesh generation.
+    T    = np.linspace(0, 1, t)                                             # Mesh generation in time.
+    dx   = x[1] - x[0]                                                      # dx is defined as the space step-length.
+    dt   = T[1] - T[0]                                                      # dt is defined as the time step-length.
+    u_ap = np.zeros([m,t])                                                  # u_ap is initialized with zeros.
+    
+    return u_ap                                                             # Return the approximated solution.
+
---
+++ Examples/Advection/Example_1.py
@@ -0,0 +1,49 @@
+""""
+Example 1 for Classical Finite Difference Schemes to solve Advection Equation.
+
+All the codes were developed by:
+    Dr. Gerardo Tinoco Guerrero
+    Universidad Michoacana de San Nicolás de Hidalgo
+    gerardo.tinoco@umich.mx
+ 
+With the funding of:
+    National Council of Humanities, Sciences and Technologies, CONAHCyT (Consejo Nacional de Humanidades, Ciencias y Tecnologías, CONAHCyT). México.
+    Coordination of Scientific Research, CIC-UMSNH (Coordinación de la Investigación Científica de la Universidad Michoacana de San Nicolás de Hidalgo, CIC-UMSNH). México
+    Aula CIMNE-Morelia. México
+ 
+Date:
+    October, 2022.
+
+Last Modification:
+    October, 2023.
+"""
+
+# Path Importation
+import sys
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+root_dir = os.path.dirname(parent_dir)
+sys.path.insert(1, root_dir)
+
+# Library Importation
+import numpy as np
+from Advection_Equation import Advection_1D_FTCS
+from Scripts.Graphs import Graph_1D_Transient
+
+
+# Problem Parameters
+m    = 11
+t    = 200
+u    = lambda x,t,a: x-a*t
+a   = 0.3
+x    = np.linspace(0, 1, m)
+T    = np.linspace(0, 1, t)
+u_ex = np.zeros([m,t])
+
+for k in range(t):
+    for i in range(m):
+        u_ex[i,k] = u(x[i], T[k], a)
+
+u_ap = Advection_1D_FTCS(m, t, u, a)
+Graph_1D_Transient(x, t, u_ap, u_ex)
