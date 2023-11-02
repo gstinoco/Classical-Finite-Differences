6d697c09cbc2fb77e8630b7b3ab9dbf1efd589be
gstinoco <gstinoco@gmail.com>
2023-11-02 09:16:44 -0600

    Graphics for Spyder.

--- Scripts/Graphs.py
+++ Scripts/Graphs.py
@@ -138,3 +138,67 @@ def Graph_2D_Transient(x, y, u_ap, u_ex):
     ax2.set_title('Theoretical Solution')
 
     plt.pause(0.1)
+
+def Graph_2D_Transient_1(x, y, u_ap, u_ex):
+    """
+    Graph_2D_Transient_1
+
+    This function graphs the approximated and theoretical solutions of the problem being solved at several time levels.
+    Both solutions are presented side by side to help perform graphical comparisons between both solutions.
+    
+    Input:
+        x           m x n           Array           Array with the x-coordinates of the nodes.
+        y           m x n           Array           Array with the y-coordinates of the nodes.
+        u_ap        m x n x t       Array           Array with the computed solution.
+        u_ex        m x n x t       Array           Array with the theoretical solution.
+    
+    Output:
+        None
+    """
+    t    = len(u_ex[0,0,:])
+    min  = u_ex.min()
+    max  = u_ex.max()
+    T    = np.linspace(0,1,t)
+    
+    
+    fig, (ax1, ax2) = plt.subplots(1, 2, subplot_kw = {"projection": "3d"}, figsize=(8, 4))
+
+    k = np.ceil(0)
+    tin = float(T[k])
+    plt.suptitle('Solution at t = %1.3f s.' %tin)
+    ax1.plot_surface(x, y, u_ap[:,:,k])
+    ax1.set_zlim([min, max])
+    ax1.set_title('Approximation')
+    ax2.plot_surface(x, y, u_ex[:,:,k])
+    ax2.set_zlim([min, max])
+    ax2.set_title('Theoretical Solution')
+    plt.show()
+
+    ax1.clear()
+    ax2.clear()
+    plt.cla()
+
+    k = np.ceil(t/2)
+    tin = float(T[k])
+    plt.suptitle('Solution at t = %1.3f s.' %tin)
+    ax1.plot_surface(x, y, u_ap[:,:,k])
+    ax1.set_zlim([min, max])
+    ax1.set_title('Approximation')
+    ax2.plot_surface(x, y, u_ex[:,:,k])
+    ax2.set_zlim([min, max])
+    ax2.set_title('Theoretical Solution')
+    plt.show()
+
+    ax1.clear()
+    ax2.clear()
+    plt.cla()
+    
+    tin = float(T[-1])
+    plt.suptitle('Solution at t = %1.3f s.' %tin)
+    ax1.plot_surface(x, y, u_ap[:,:,-1])
+    ax1.set_zlim([min, max])
+    ax1.set_title('Approximation')
+    ax2.plot_surface(x, y, u_ex[:,:,-1])
+    ax2.set_zlim([min, max])
+    ax2.set_title('Theoretical Solution')
+    plt.show()
